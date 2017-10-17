99f3999b24a78dd05172e949b79cb8624e3998f4
emanueledimarco <emanuele.dimarco@gmail.com>
2017-10-17 15:40:39 +0200

    adding basic estimators (amplitude and time)

--- ScopeAnalysis/txt2pulse.py
+++ ScopeAnalysis/txt2pulse.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python
 
 from math import *
-import re
-import os, os.path
+import re, os, os.path, operator
 from array import array
+from collections import OrderedDict
 
 from PlotUtils import customROOTstyle
 
@@ -16,6 +16,11 @@ sys.argv = args
 ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
+class GraphVal:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
 class PulseTxtFile:
     def __init__(self,txtFile,options=None):
         if "C3" in txtFile: self.channel = "PMT"
@@ -24,7 +29,9 @@ class PulseTxtFile:
         self.x = []
         self.y = []
         file = open(txtFile,"r")
-        if not file: raise RuntimeError, "Cannot open "+txtFile+"\n"
+        if not file: 
+            self.validpulse = False
+            raise RuntimeError, "Cannot open "+txtFile+"\n"
         for line in file:
           try:
             line = line.strip()
@@ -32,12 +39,15 @@ class PulseTxtFile:
             fields = line.split()
             self.x.append(10e6*float(fields[0]))
             self.y.append(float(fields[1]))
+            self.validpulse=True
           except ValueError:
               print "Error parsing cut line [%s]" % line.strip()
+              self.validpulse=False
               raise
         self.options = options
 
     def getPulse(self,averageNsamples=None):
+        if not self.validpulse: return None
         if averageNsamples:
             xav=[]; yav=[]
             i=0
@@ -48,10 +58,25 @@ class PulseTxtFile:
                 yav.append(sum(self.y[imin:imax])/len(self.y[imin:imax]))
                 #print "averaging imin=%d,imax=%d,xav=%f,yav=%f" % (imin,imax,xav[-1],yav[-1])
             pulse = ROOT.TGraph(len(xav),array('f',xav),array('f',yav))
+            self.pedestal = 0.5*(yav[0]+yav[-1])
+            self.xy=[GraphVal(xav[i],yav[i]) for i in xrange(len(xav))]
+            self.xy.sort(key = lambda point : point.y) 
         else:
+            self.xy=[GraphVal(self.x[i],self.y[i]) for i in xrange(len(self.x))]
+            self.xy.sort(key = lambda point : point.y) 
             pulse = ROOT.TGraph(len(self.x),array('f',self.x),array('f',self.y))
+            self.pedestal = 0.5*(self.y[0]+self.y[-1])
         pulse.SetName(""); pulse.SetTitle("")
+        self.pulse = pulse
         return pulse
+
+    def amplitude(self):
+        if not hasattr(self,'pulse'): self.getPulse(options.ngroup)
+        return self.xy[0].y-self.pedestal
+
+    def time(self):
+        if not hasattr(self,'pulse'): self.getPulse(options.ngroup)
+        return self.xy[0].x
 
     def plot(self,saveName,doWide=False,extensions="pdf"):
         plotformat = (1200,600) if doWide else (600,600)
@@ -62,6 +87,7 @@ class PulseTxtFile:
         customROOTstyle()
         c1 = ROOT.TCanvas(saveName+"_canvas",saveName,plotformat[0],height)
         g = self.getPulse(options.ngroup)
+        print "Amplitude = ",self.amplitude(),"  Time = ",self.time()
         colors = {"calorimeter":ROOT.kRed, "PMT":ROOT.kBlue} 
         g.SetLineColor(colors[self.channel])
         g.Draw("AL")
@@ -75,7 +101,7 @@ if __name__ == "__main__":
     from optparse import OptionParser
     parser = OptionParser(usage="%prog [options] file.txt")
     parser.add_option("--print", dest="printPlots", type="string", default="png,pdf,txt", help="print out plots in this format or formats (e.g. 'png,pdf,txt')");
-    parser.add_option("--ngroup", dest="ngroup", type=int, default=1, help="average the pulse every ngroup samples");
+    parser.add_option("--ngroup", dest="ngroup", type=int, default=None, help="average the pulse every ngroup samples");
 
     (options, args) = parser.parse_args()
     PulsePlot = PulseTxtFile(args[0],options)
