69b1add68fe18f7acd1415b88b9ee3ed5087743c
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-10-19 01:28:33 +0200

    first rough plot

--- ScopeAnalysis/plots.py
+++ ScopeAnalysis/plots.py
@@ -20,29 +20,51 @@ class PlotMaker:
     def beginJob(self):
         self.tfile = ROOT.TFile.Open(self.infile)
         self.tree = self.tfile.Get("Events")
-        
+
     def endJob(self):
         self.tfile.Close()
 
     def secondaryArrivalTime(self):
         gr = ROOT.TGraphErrors(len(_xpos))
+        gr.SetTitle("")
         point=0
         for run,xpos in _xpos.iteritems():
             cut = "nPeaks==2 && run==%d" % run
-            print cut
+            if ROOT.gROOT.FindObject("h_dt") != None: ROOT.gROOT.FindObject("h_dt").Delete()
             h_dt = ROOT.TH1D("h_dt","h_dt",200,10,100)
-            self.tree.Draw("amplitude_pmt[1]-amplitude_pmt[0]>>h_dt",cut)
+            self.tree.Draw("time_pmt[1]-time_pmt[0]>>h_dt",cut)
             mean,rms = h_dt.GetMean(),h_dt.GetRMS()
-            print xpos,"  ",mean
             gr.SetPoint(point,xpos,mean)
             gr.SetPointError(point,0,rms)
+            point +=1
         c1 = ROOT.TCanvas("dist_canvas","dist",600,600)
+        ROOT.gStyle.SetOptFit(11111)
+        gr.SetMarkerStyle(21)
+        gr.SetMarkerSize(0.4)
         gr.Draw("APE")
+        gr.GetXaxis().SetNdivisions(510)
+        gr.GetXaxis().SetTitleFont(42)
+        gr.GetXaxis().SetTitleSize(0.05)
+        gr.GetXaxis().SetTitleOffset(0.9)
+        gr.GetXaxis().SetLabelFont(42)
+        gr.GetXaxis().SetLabelSize(0.05)
+        gr.GetXaxis().SetLabelOffset(0.007)
+        gr.GetYaxis().SetTitleFont(42)
+        gr.GetYaxis().SetTitleSize(0.05)
+        gr.GetYaxis().SetTitleOffset(0.9)
+        gr.GetYaxis().SetLabelFont(42)
+        gr.GetYaxis().SetLabelSize(0.05)
+        gr.GetYaxis().SetLabelOffset(0.007)
+        gr.GetXaxis().SetTitle("x position [cm]")
+        gr.GetYaxis().SetTitle("PMT #Deltat [ns]")
+        gr.Fit("pol1")
+        gr.Draw("APE")
+
         c1.Print("distance.pdf")
 
 if __name__ == "__main__":
 
-    pm = PlotMaker("reco_all.root")
+    pm = PlotMaker("reco.root")
     pm.beginJob()
     pm.secondaryArrivalTime()
     pm.endJob()
