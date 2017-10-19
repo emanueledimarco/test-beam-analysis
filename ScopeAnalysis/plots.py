9389c7a9a409f5c9d4ff3fad789284956256b973
emanueledimarco <emanuele.dimarco@gmail.com>
2017-10-19 18:37:50 +0200

    temporary fix valid only for tdrift

--- ScopeAnalysis/plots.py
+++ ScopeAnalysis/plots.py
@@ -9,7 +9,7 @@ sys.argv = args
 ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-from PlotUtils import customROOTstyle
+from PlotUtils import customROOTstyle, doSpam
 
 _xpos = { 206:294.2, 207:314.2, 208:334.2, 209:354.2, 210:374.2, 211:394.2, 212:414.2, 213:434.2, 214:454.2 }
 
@@ -30,24 +30,30 @@ class PlotMaker:
         point=0
         for run,xpos in _xpos.iteritems():
             if run==206 and "amplitude" in var: continue # saturation: too close to cathode
-            cut = "nPeaks==2 && run==%d" % run
+            cut = "nPeaks==2 && run==%d && abs(amplitude_pmt[0])<abs(amplitude_pmt[1])" % run
             if ROOT.gROOT.FindObject("h_dt") != None: ROOT.gROOT.FindObject("h_dt").Delete()
             self.tree.Draw('{var}>>h_dt(100,{min},{max})'.format(var=var,min=range[0],max=range[1]),cut)
             h_dt=ROOT.gROOT.FindObject("h_dt")
             mean,rms = h_dt.GetMean(),h_dt.GetMeanError()
-            gr.SetPoint(point,xpos,mean)
+            gr.SetPoint(point,(_xpos[214]-xpos+10)/10.,mean)
             gr.SetPointError(point,0,rms)
             point +=1
-        customROOTstyle()
+        #customROOTstyle()
         c1 = ROOT.TCanvas("dist_canvas","dist",600,600)
-        ROOT.gStyle.SetOptFit(11111)
-        gr.SetMarkerStyle(21)
-        gr.SetMarkerSize(0.4)
+        ROOT.gStyle.SetOptFit(0)
+        gr.SetMarkerStyle(8)
+        gr.SetMarkerSize(1.0)
         gr.Draw("APE")
-        gr.GetXaxis().SetTitle("x position [cm]")
+        gr.GetXaxis().SetTitle("distance [cm]")
         gr.GetYaxis().SetTitle(range[2])
         gr.Fit("pol1")
         gr.Draw("APE")
+        doSpam("Primary-Secondary scintillation",0.08,.95,.50,.95, align=12, textSize=0.033)
+        doSpam("n(electrons)=400",0.50,.95,.90,.95, align=32, textSize=0.033)
+        pol = gr.GetFunction("pol1")
+        vel,err = pol.GetParameter(1),pol.GetParError(1)
+        print vel,"  ",err
+        doSpam("V_{drift} = %.3f #pm %.3f cm/#mus"%(vel,err),0.55,.25,.85,.25, align=32, textSize=0.033) 
         for ext in self.options.printPlots.split(','):
             plotname=var.replace("[",""); plotname=plotname.replace("]","")
             plotname=plotname.replace("(",""); plotname=plotname.replace(")","");
@@ -62,7 +68,7 @@ if __name__ == "__main__":
 
     pm = PlotMaker("reco_all.root",options)
     pm.beginJob()
-    vars = {'time_pmt[1]-time_pmt[0]':(10,100,'PMT #Deltat [ns]'),
+    vars = {'time_pmt[1]-time_pmt[0]':(10,100,'PMT #Deltat [#mus]'),
             'abs(amplitude_pmt[0])/abs(amplitude_pmt[1])/abs(amplitude_calo)':(0,1,'Primary/Secondary/Calo Ampl'),
             'abs(amplitude_pmt[0])/abs(amplitude_pmt[1])':(0,1,'Primary/Secondary Ampl'),
             'abs(amplitude_pmt[0])/abs(amplitude_calo)':(0,0.15,'Primary/Calo Ampl'),
