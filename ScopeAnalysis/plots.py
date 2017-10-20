5b0acb6172dd4c6fdfc4a93cb842600493dcb9ed
emanueledimarco <emanuele.dimarco@gmail.com>
2017-10-20 13:21:37 +0200

    remove the PMT offset in the plot

--- ScopeAnalysis/plots.py
+++ ScopeAnalysis/plots.py
@@ -10,6 +10,7 @@ ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 from PlotUtils import customROOTstyle, doSpam
+from array import array
 
 _xpos = { 206:294.2, 207:314.2, 208:334.2, 209:354.2, 210:374.2, 211:394.2, 212:414.2, 213:434.2, 214:454.2 }
 
@@ -26,7 +27,6 @@ class PlotMaker:
 
     def secondaryVarPlot(self,var,range):
         gr = ROOT.TGraphErrors(len(_xpos)-1 if "amplitude" in var else len(_xpos))
-        gr.SetTitle("")
         point=0
         for run,xpos in _xpos.iteritems():
             if run==206 and "amplitude" in var: continue # saturation: too close to cathode
@@ -38,20 +38,37 @@ class PlotMaker:
             gr.SetPoint(point,(_xpos[214]-xpos+10)/10.,mean)
             gr.SetPointError(point,0,rms)
             point +=1
-        #customROOTstyle()
-        c1 = ROOT.TCanvas("dist_canvas","dist",600,600)
+        csize=600
+        c1 = ROOT.TCanvas("dist_canvas","dist",csize,csize)
+        c1.SetWindowSize(csize + (csize - c1.GetWw()), csize + (csize - c1.GetWh()));
+        c1.SetLeftMargin(0.15); c1.SetBottomMargin(0.15)
         ROOT.gStyle.SetOptFit(0)
-        gr.SetMarkerStyle(8)
-        gr.SetMarkerSize(1.0)
         gr.Draw("APE")
-        gr.GetXaxis().SetTitle("distance [cm]")
-        gr.GetYaxis().SetTitle(range[2])
         gr.Fit("pol1")
-        gr.Draw("APE")
-        doSpam("Primary-Secondary scintillation",0.08,.95,.50,.95, align=12, textSize=0.033)
-        doSpam("n(electrons)=400",0.50,.95,.90,.95, align=32, textSize=0.033)
         pol = gr.GetFunction("pol1")
         vel,err = pol.GetParameter(1),pol.GetParError(1)
+        def removeOffset(gr,a,b):
+            print a, "   ",b
+            xp=[x+b/a for x in gr.GetX()]
+            grshift=ROOT.TGraphErrors(len(xp),array('f',xp),array('f',gr.GetY()),array('f',gr.GetEX()),array('f',gr.GetEY()))
+            return grshift
+        grp=removeOffset(gr,vel,pol.GetParameter(0))
+        customROOTstyle()
+        grp.SetTitle("")
+        grp.SetMarkerStyle(8)
+        grp.SetMarkerSize(1.0)
+        grp.Draw("APE")
+        grp.Fit("pol1")
+        grp.GetXaxis().SetLimits(0,25)
+        grp.GetYaxis().SetRangeUser(0,100)
+        grp.GetYaxis().SetLimits(0,100)
+        grp.GetXaxis().SetTitle("distance [cm]")
+        grp.GetYaxis().SetTitle(range[2])
+        grp.GetYaxis().SetTitleOffset(1.3)
+        grp.GetXaxis().SetTitleOffset(1.3)
+        grp.Draw("APE")
+        doSpam("Primary-Secondary scintillation",0.13,.95,.60,.95, align=12, textSize=0.033)
+        doSpam("n(electrons)=400",0.50,.95,.90,.95, align=32, textSize=0.033)
         print vel,"  ",err
         doSpam("V_{drift} = %.3f #pm %.3f cm/#mus"%(vel,err),0.55,.25,.85,.25, align=32, textSize=0.033) 
         for ext in self.options.printPlots.split(','):
@@ -69,11 +86,12 @@ if __name__ == "__main__":
     pm = PlotMaker("reco_all.root",options)
     pm.beginJob()
     vars = {'time_pmt[1]-time_pmt[0]':(10,100,'PMT #Deltat [#mus]'),
-            'abs(amplitude_pmt[0])/abs(amplitude_pmt[1])/abs(amplitude_calo)':(0,1,'Primary/Secondary/Calo Ampl'),
-            'abs(amplitude_pmt[0])/abs(amplitude_pmt[1])':(0,1,'Primary/Secondary Ampl'),
-            'abs(amplitude_pmt[0])/abs(amplitude_calo)':(0,0.15,'Primary/Calo Ampl'),
-            'abs(amplitude_pmt[1])':(0,0.1,'Secondary Ampl [V]'),
-            'abs(amplitude_pmt[1])/abs(amplitude_calo)':(0,0.5,'Secondary/Calo Ampl') }
+#             'abs(amplitude_pmt[0])/abs(amplitude_pmt[1])/abs(amplitude_calo)':(0,1,'Primary/Secondary/Calo Ampl'),
+#             'abs(amplitude_pmt[0])/abs(amplitude_pmt[1])':(0,1,'Primary/Secondary Ampl'),
+#             'abs(amplitude_pmt[0])/abs(amplitude_calo)':(0,0.15,'Primary/Calo Ampl'),
+#             'abs(amplitude_pmt[1])':(0,0.1,'Secondary Ampl [V]'),
+#             'abs(amplitude_pmt[1])/abs(amplitude_calo)':(0,0.5,'Secondary/Calo Ampl')
+            }
     for var,range in vars.iteritems():
         pm.secondaryVarPlot(var,range)
 
